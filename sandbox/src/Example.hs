a2c1625247e2e7daaba29211dd44bdd344a33f84
Kai-Oliver Prott <kai.prott@hotmail.de>
2020-11-19 19:32:12 +0100

    Restore sandbox example

--- sandbox/src/Example.hs
+++ sandbox/src/Example.hs
@@ -4,11 +4,9 @@ module Example where
 
 import Plugin.CurryPlugin.Prelude
 
-newtype I a = I a
-
-bind :: I a -> (a -> I b) -> I b
-bind (I a) f = f a
-
-applyBoth :: I (I (forall c. I c -> I c) -> I (I (I a, I b) -> I (I a, I b)))
--- Nondet ((forall a. a)
-applyBoth = I (\f -> I (\t -> f `bind` (\f' -> t `bind` (\(a, b) -> I (f' a, f' b)))))
+permutations :: [a] -> [a]
+permutations []     = []
+permutations (x:xs) = insert x (permutations xs)
+  where
+    insert e []     = [e]
+    insert e (y:ys) = (e:y:ys) ? (y : insert e ys)
