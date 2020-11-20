339cb2b4637762e33541969e1b14762229a9a3df
Kai-Oliver Prott <kai.prott@hotmail.de>
2020-11-20 10:24:08 +0100

    Fix sandbox example

--- sandbox/src/Example.hs
+++ sandbox/src/Example.hs
@@ -1,5 +1,5 @@
+{-# OPTIONS_GHC -fplugin Plugin.CurryPlugin #-}
 {-# LANGUAGE NoImplicitPrelude              #-}
-{-# LANGUAGE RankNTypes #-}
 module Example where
 
 import Plugin.CurryPlugin.Prelude
