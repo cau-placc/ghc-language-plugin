845b86b34eae35c6f16347fd4c475d076378881e
Kai-Oliver Prott <kai.prott@hotmail.de>
2020-11-25 16:57:43 +0100

    Extend the instance test for standalone deriving.

--- test-examples/InstanceExport.hs
+++ test-examples/InstanceExport.hs
@@ -1,11 +1,17 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude              #-}
+{-# LANGUAGE StandaloneDeriving             #-}
 {-# OPTIONS_GHC -fplugin Plugin.CurryPlugin #-}
 module InstanceExport where
 
 import Plugin.CurryPlugin.Prelude
 
 newtype Id a = Id a
-  deriving Show
+
+data Phantom a = Phantom
+
+deriving instance Show (Phantom a)
+
+deriving instance Show a => Show (Id a)
 
 instance Functor Id where
   fmap f (Id a) = Id (f a)
