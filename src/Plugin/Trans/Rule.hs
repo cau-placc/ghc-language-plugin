4d3b594e4f8f49db5ace931b73547e387d51a096
Kai-Oliver Prott <kai.prott@hotmail.de>
2020-12-14 16:53:09 +0100

    Simplify result of rule translations

--- src/Plugin/Trans/Rule.hs
+++ src/Plugin/Trans/Rule.hs
@@ -5,11 +5,14 @@ import Data.Maybe
 import GHC.Hs
 import GHC.Plugins
 import GHC.Tc.Types
-import GHC.Tc.Utils.Monad
 import GHC.Tc.Types.Constraint
+import GHC.Tc.Types.Origin
+import GHC.Tc.Types.Evidence
+import GHC.Tc.Utils.TcType
+import GHC.Tc.Utils.Monad
 import GHC.Tc.Solver
-import GHC.Tc.Solver.Monad
 import GHC.Data.Bag
+import GHC.Core.Predicate
 
 import Plugin.Trans.Expr
 import Plugin.Trans.Type
@@ -18,26 +21,36 @@ import Plugin.Trans.Util
 liftRule :: TyConMap -> LRuleDecl GhcTc -> TcM (LRuleDecl GhcTc)
 liftRule tcs r@(L l (HsRule x nm act tvs tmvs lhs rhs)) = do
   tmvs' <- mapM (liftRuleBndr tcs) tmvs
-  ((lhs', rhs'), constraints)
-    <- captureConstraints ((,) <$> liftMonadicExpr [] tcs lhs
-                               <*> liftMonadicExpr [] tcs rhs)
+  (lhs', constraints) <- captureConstraints (liftMonadicExpr [] tcs lhs)
 
-  (simplified, _) <- runTcS (solveWantedsAndDrop constraints >>= zonkWC)
-
-  dicts <- case simplified of
+  evs <- case constraints of
     WC wanted impl holes
       | isEmptyBag impl && isEmptyBag holes && allBag isWantedCt wanted
       -> return $ mapMaybe extractEvVar $ bagToList wanted
     _ -> panicAny "Lifting of rule lead to unexpected constraints" r
+  let dicts = map (noLoc . RuleBndr noExtField . noLoc) evs
 
-  return (L l (HsRule x nm act tvs (tmvs' ++ dicts) lhs' rhs'))
+  lclEnv <- getLclEnv
+
+  (L loc rhs', WC wanted impl holes) <-
+    captureConstraints (liftMonadicExpr [] tcs rhs)
+  let ctloc = mkGivenLoc topTcLevel UnkSkol lclEnv
+  let given = listToBag $ mkGivens ctloc evs
+  rhs'' <- L loc . flip mkHsWrap rhs' . mkWpLet . EvBinds <$>
+    simplifyTop (WC (wanted `unionBags` given) impl holes)
+
+  return (L l (HsRule x nm act tvs (filter isNoDict tmvs' ++ dicts) lhs' rhs''))
   where
-    extractEvVar :: Ct -> Maybe (LRuleBndr GhcTc)
+    extractEvVar :: Ct -> Maybe Var
     extractEvVar (CQuantCan _) = Nothing
     extractEvVar ct            = case cc_ev ct of
-      CtWanted _ (EvVarDest v) _ _
-        -> Just (noLoc (RuleBndr noExtField (noLoc v)))
-      _ -> Nothing
+                                  CtWanted _ (EvVarDest v) _ _
+                                    -> Just v
+                                  _ -> Nothing
+
+    isNoDict :: LRuleBndr GhcTc -> Bool
+    isNoDict (L _ (RuleBndr _ (L _ v))) = not $ isDictTy (varType v)
+    isNoDict _                          = False
 
 liftRuleBndr :: TyConMap -> LRuleBndr GhcTc -> TcM (LRuleBndr GhcTc)
 liftRuleBndr tcs   (L l1 (RuleBndr    x (L l2 v) ))
